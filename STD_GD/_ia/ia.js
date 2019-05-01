a87333a5ab95438e2a0fd7f00bde8e8e6d56ac54
Publisher Changi Jo <36979670+jochanggi@users.noreply.github.com>
2019-05-02 01:29:33 +0900

    20190502

--- STD_GD/_ia/ia.js
+++ STD_GD/_ia/ia.js
@@ -25,14 +25,14 @@ var ia = {
 			$(this).load(ajaxURL+file+'.html', function(){
 				_this.cal('#gIA'+i);
 				_this.url('#gIA'+i);
-			});
 
-			if (lens - 1 == i){
-				iaUI.spyScroll.init();
-				setTimeout(function(){
-					iaUI.accordion.init();
-				},200)
-			}
+				if (lens - 1 == i){
+					iaUI.spyScroll.init();
+					setTimeout(function(){
+						iaUI.accordion.init();
+					},200)
+				}
+			});
 		})
 	},
 	cal : function(obj){
@@ -104,8 +104,16 @@ var ia = {
 	},
 	url : function(obj){
 		$(obj).find('td.col-url').each(function(){
-			var text = $(this).text();
-			$(this).empty().append('<a href="'iaURL+text+'" target="_blank">/'+text+'</a>');
+			var src = iaURL + $(this).text();
+			$(this).empty().append('<a href="'+src+'" target="_blank">'+src+'</a>');
+			/* Preview 모드 */
+			if ($(obj).find('.ia-iframe-list').length){
+				var depth1 = $(this).prevAll('.col-1depth').text();
+				var depth2 = $(this).prevAll('.col-2depth').text();
+				var itemTit = depth1 + ' > ' + depth2;
+				var $objFrameList = $(obj).find('.ia-iframe-list');
+				$objFrameList.append('<div class="item"><iframe src="'+src+'" frameborder="0"></iframe><div class="item-tit"><a href="'+src+'" target="_blank" class="item-link">'+itemTit+' (새창보기)</a></div></div>');
+			}
 		})
 	},
 }
@@ -150,7 +158,6 @@ var iaUI = {
 			var _this = this;
 			_this.event();
 			_this.action($(this.eleModule).eq(1).find(this.eleBody));
-			_this.action($(this.eleModule).eq(2).find(this.eleBody));
 		},
 		event : function(){
 			var _this = this;
@@ -164,6 +171,18 @@ var iaUI = {
 			} else {
 				$this.css('height', '0');
 			}
+		},
+	},
+	preview : {
+		eleThumb : '.ia-tab-content.type-thumb',
+		eleList : '.ia-tab-content.type-list',
+		thumb : function(){
+			$(this.eleThumb).addClass('is-visible');
+			$(this.eleList).removeClass('is-visible');
+		},
+		list : function(){
+			$(this.eleThumb).removeClass('is-visible');
+			$(this.eleList).addClass('is-visible');
 		},
 	}
 }
